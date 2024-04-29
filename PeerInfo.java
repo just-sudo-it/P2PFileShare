15d07a71e621d8f3e6477a69e1fe4d381a01cfb7
just-sudo-it <57672397+just-sudo-it@users.noreply.github.com>
2024-04-29 16:25:39 +0300

    Create PeerInfo.java

---
+++ PeerInfo.java
@@ -0,0 +1,12 @@
+class PeerInfo {
+    String username;
+    String password; // Placeholder for password management
+    String ip;
+    int port;
+
+    public PeerInfo(String username, String ip, int port) {
+        this.username = username;
+        this.ip = ip;
+        this.port = port;
+    }
+}
