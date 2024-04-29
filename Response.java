a3e4d1b1d9595b421642de2aa9fd161c2600beaf
just-sudo-it <57672397+just-sudo-it@users.noreply.github.com>
2024-04-29 16:26:29 +0300

    Create Response.java

---
+++ Response.java
@@ -0,0 +1,17 @@
+class Response implements Serializable {
+    private String status;
+    private String message;
+
+    public Response(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
